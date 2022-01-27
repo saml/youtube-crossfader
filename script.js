2597d1ea27289a4574fe4205cbecde31a7010e08
saml <saml>
2022-01-27 15:00:34 -0500

    asdf

--- script.js
+++ script.js
@@ -1,30 +1,67 @@
 class YoutubeCrossfader {
-  constructor(players) {
-    this.players = players;
+  constructor() {
+    this.currentPlayer = null;
+    this.previousPlayer = null;
   }
 
-  static initPlayer(player) {
-    // player.pauseVideo();
-    // player.setLoop(true);
-  }
-
-  static onPlayerReady(event) {
+  onPlayerReady(event) {
     event.target.setLoop(true);
   }
 
-  static onPlayerStateChange(event) {
+  onPlayerStateChange(event) {
     if (event.data === YT.PlayerState.PLAYING) {
-      console.log(event.target);
+      if (this.currentPlayer) {
+        console.log('Current player is being switched. Previous player will be', this.currentPlayer);
+        this.previousPlayer = this.currentPlayer;
+      }
+      this.currentPlayer = event.target;
     }
+  }
+
+  static crossfade({
+    firstPlayer,
+    secondPlayer,
+    durationSecs
+  }) {
+    if (!firstPlayer) {
+      // noting to crossfade from. use fake player.
+      firstPlayer = {
+        getVolume: () => 100,
+        pauseVideo: () => null,
+        setVolume: () => null,
+      };
+    }
+    const targetVolume = firstPlayer.getVolume();
+    const intervalMSecs = 500;
+    const delta = targetVolume / (durationSecs * 1000 / intervalMSecs);
+    const intervalId = setInterval(() => {
+      const firstVolume = firstPlayer.getVolume();
+      if (firstVolume <= 0) {
+        firstPlayer.pauseVideo();
+        secondPlayer.setVolume(targetVolume);
+        clearInterval(intervalId);
+        return;
+      }
+      const secondVolume = secondPlayer.getVolume();
+      console.log('Crossfading', firstVolume, delta, secondVolume);
+      firstPlayer.setVolume(firstVolume - delta);
+      secondPlayer.setVolume(secondVolume + delta);
+    }, intervalMSecs);
   }
 
   static build({
     videos,
     height,
-    width
+    width,
+    crossfadeDurationSecs,
   }) {
+    const youtubeCrossfader = new YoutubeCrossfader();
     const players = [];
-    videos.forEach(([videoId, playerId]) => {
+    videos.forEach(({
+      videoId,
+      playerId,
+      ui
+    }) => {
       const player = new YT.Player(playerId, {
         height,
         width,
@@ -35,26 +72,58 @@ class YoutubeCrossfader {
           loop: 1,
         },
         events: {
-          onReady: YoutubeCrossfader.onPlayerReady,
-          onStateChange: YoutubeCrossfader.onPlayerStateChange,
+          onReady: youtubeCrossfader.onPlayerReady.bind(youtubeCrossfader),
+          onStateChange: youtubeCrossfader.onPlayerStateChange.bind(youtubeCrossfader),
         },
       });
+
+      ui.crossfadeButton.addEventListener('click', () => {
+        player.setVolume(0);
+
+        YoutubeCrossfader.crossfade({
+          firstPlayer: youtubeCrossfader.currentPlayer,
+          secondPlayer: player,
+          durationSecs: crossfadeDurationSecs,
+        });
+
+        player.playVideo();
+      });
+
+      // agumenting YT Player object
+      player.youtubeCrossfader = {
+        videoId,
+        playerId,
+        ui,
+      };
       players.push(player);
     });
-    return new YoutubeCrossfader(players);
+    return youtubeCrossfader;
   }
+}
+
+
+function buildVideoSettings(videoId, playerId, crossfadeButtonId) {
+  return {
+    videoId,
+    playerId,
+    ui: {
+      crossfadeButton: document.getElementById(crossfadeButtonId),
+    },
+  };
 }
 
 function onYouTubeIframeAPIReady() {
   const height = 390;
   const width = 640;
+  const crossfadeDurationSecs = 15;
   const youtubeCrossfader = YoutubeCrossfader.build({
     videos: [
-      ['nmq9cOK-mIQ', 'player1'],
-      ['SMosjLl5AwM', 'player2'],
-      ['K9caQYMhaC4', 'player3'],
+      buildVideoSettings('nmq9cOK-mIQ', 'player1', 'crossfade1'),
+      buildVideoSettings('SMosjLl5AwM', 'player2', 'crossfade2'),
+      buildVideoSettings('K9caQYMhaC4', 'player3', 'crossfade3'),
     ],
     height,
     width,
+    crossfadeDurationSecs,
   });
 }
